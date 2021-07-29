59655a142c0d23bbefbb48ba10d5ad9a47744c69
eliaraguso <elia.raguso@gmail.com>
2021-07-29 14:43:52 +0200

    inizio creazione script

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// ) Definire il prezzo per Km del biglietto
+var prezzoKm = 0.21;
+
+// ) Chiedere all'utente il numero di Km che percorrerà il passeggero
+var kmViaggio = prompt("Inserisci il chilometraggio del viaggio");
+
+// ) Chiedere all'utente l'età del passeggero
+var etaPasseggero = prompt("Inserisci l'età del passeggero");
+
+// ) Calcolare il prezzo del biglietto
+var prezzoBiglietto = (kmViaggio * prezzoKm1);
+
+// ) Restituire il prezzo del biglietto
+alert ("Il prezzo del tuo biglietto è di " + prezzoBiglietto + " euro");
+
