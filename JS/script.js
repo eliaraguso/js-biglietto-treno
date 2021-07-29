6ea57ea44f4358d62e4bfe98a9b1f28e1e0ed9da
eliaraguso <elia.raguso@gmail.com>
2021-07-29 16:06:55 +0200

    aggiunto colore al .box tramite css

--- JS/script.js
+++ JS/script.js
@@ -18,10 +18,10 @@ var prezzoBigliettoO65 = ((prezzoBiglietto) - ((prezzoBiglietto) * 0.4))
 
 // ) Restituire il prezzo del biglietto
 if (etaPasseggero < 18) {
-    alert ("Hey tu!!! Si proprio tu!!!! Hai meno di 18 anni e ti spetta un bello sconto del 20% sul totale!!!! Il prezzo del tuo biglietto è di " + prezzoBigliettoU18.toFixed(2) + " euro");
+    document.getElementById("biglietto").innerHTML = ("Hey tu!!! Si proprio tu!!!! Hai meno di 18 anni e ti spetta un bello sconto del 20% sul totale!!!! Il prezzo del tuo biglietto è di " + prezzoBigliettoU18.toFixed(2) + " euro");
 } else if (etaPasseggero > 65) {
-    alert ("Hey tu!!! Si proprio tu!!!! Hai più di 65 anni e ti spetta un bello sconto del 40% sul totale!!!! Il prezzo del tuo biglietto è di " + prezzoBigliettoO65.toFixed(2) + " euro");
+    document.getElementById("biglietto").innerHTML = ("Hey tu!!! Si proprio tu!!!! Hai più di 65 anni e ti spetta un bello sconto del 40% sul totale!!!! Il prezzo del tuo biglietto è di " + prezzoBigliettoO65.toFixed(2) + " euro");
 } else {
-    alert ("Il prezzo del tuo biglietto è di " + prezzoBiglietto.toFixed(2) + " euro");
+    document.getElementById("biglietto").innerHTML = ("Il prezzo del tuo biglietto è di " + prezzoBiglietto.toFixed(2) + " euro");
 }
 
