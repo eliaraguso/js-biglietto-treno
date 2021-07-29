7185718fc1f7568f5f78c5d642ac07e4dcd8ba0f
eliaraguso <elia.raguso@gmail.com>
2021-07-29 15:01:58 +0200

    completato script che calcola i vari prezzi del biglietto in base all'età

--- JS/script.js
+++ JS/script.js
@@ -7,9 +7,21 @@ var kmViaggio = prompt("Inserisci il chilometraggio del viaggio");
 // ) Chiedere all'utente l'età del passeggero
 var etaPasseggero = prompt("Inserisci l'età del passeggero");
 
-// ) Calcolare il prezzo del biglietto
-var prezzoBiglietto = (kmViaggio * prezzoKm1);
+// ) Calcolare il prezzo del biglietto intero
+var prezzoBiglietto = (kmViaggio * prezzoKm);
+
+// ) Calcolare il prezzo del biglietto Under18
+var prezzoBigliettoU18 = ((prezzoBiglietto) - ((prezzoBiglietto) * 0.2))
+
+// ) Calcolare il prezzo del biglietto Under18
+var prezzoBigliettoO65 = ((prezzoBiglietto) - ((prezzoBiglietto) * 0.4))
 
 // ) Restituire il prezzo del biglietto
-alert ("Il prezzo del tuo biglietto è di " + prezzoBiglietto + " euro");
+if (etaPasseggero < 18) {
+    alert ("Il prezzo del tuo biglietto è di " + prezzoBigliettoU18 + " euro");
+} else if (etaPasseggero > 65) {
+    alert ("Il prezzo del tuo biglietto è di " + prezzoBigliettoO65 + " euro");
+} else {
+    alert ("Il prezzo del tuo biglietto è di " + prezzoBiglietto + " euro");
+}
 
